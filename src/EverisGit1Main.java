14f58f015f480ff30db15ba14803073bca9b1ef4
agadelao <agadelao@everis.com>
2021-03-25 11:00:13 +0100

    version1

---
+++ src/EverisGit1Main.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * 
+ * @author agadelao
+ *
+ */
+
+public class EverisGit1Main {
+
+	/**
+	 * Metodo principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Hola Mundo");
+
+	}
+
+}
